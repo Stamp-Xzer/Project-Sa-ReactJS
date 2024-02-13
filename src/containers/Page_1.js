73d79adf949e18a2c423a2240f07fd7d3a5f1775
Stamp-Xzer <147483678+Stamp-Xzer@users.noreply.github.com>
2024-02-13 22:09:14 +0700

    .

--- src/containers/Page_1.js
+++ src/containers/Page_1.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import withAuth from "../containers/error/auth.js"; // Adjusted import path for withAuth
+import { Link } from "react-router-dom/cjs/react-router-dom.min.js";
 
 class Page_1 extends Component {
   render() {
@@ -13,6 +14,7 @@ class Page_1 extends Component {
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/6/67/KU_SubLogo.png"
                   className="img-fluid"
+                  alt="KU logo"
                 ></img>
               </a>
             </li>
@@ -21,7 +23,9 @@ class Page_1 extends Component {
                 <div className="navbar-item-inner-icon-wrapper flexbox">
                   <ion-icon name="home-outline"></ion-icon>
                 </div>
-                <span className="link-text">Home</span>
+                <Link to="/page1">
+                  <span className="link-text">Home</span>
+                </Link>
               </a>
             </li>
             <li className="navbar-item flexbox-left">
@@ -29,7 +33,9 @@ class Page_1 extends Component {
                 <div className="navbar-item-inner-icon-wrapper flexbox">
                   <ion-icon name="folder-open-outline"></ion-icon>
                 </div>
-                <span className="link-text">Projects</span>
+                <Link to="/page2">
+                  <span className="link-text">Profile</span>
+                </Link>
               </a>
             </li>
             <li className="navbar-item flexbox-left">
@@ -37,7 +43,9 @@ class Page_1 extends Component {
                 <div className="navbar-item-inner-icon-wrapper flexbox">
                   <ion-icon name="pie-chart-outline"></ion-icon>
                 </div>
-                <span className="link-text">Dashboard</span>
+                <Link to="/page3">
+                  <span className="link-text">Subject</span>
+                </Link>
               </a>
             </li>
             <li className="navbar-item flexbox-left">
@@ -45,41 +53,13 @@ class Page_1 extends Component {
                 <div className="navbar-item-inner-icon-wrapper flexbox">
                   <ion-icon name="people-outline"></ion-icon>
                 </div>
-                <span className="link-text">Team</span>
-              </a>
-            </li>
-            <li className="navbar-item flexbox-left">
-              <a className="navbar-item-inner flexbox-left">
-                <div className="navbar-item-inner-icon-wrapper flexbox">
-                  <ion-icon name="chatbubbles-outline"></ion-icon>
-                </div>
-                <span className="link-text">Support</span>
-              </a>
-            </li>
-            <li className="navbar-item flexbox-left">
-              <a className="navbar-item-inner flexbox-left">
-                <div className="navbar-item-inner-icon-wrapper flexbox">
-                  <ion-icon name="settings-outline"></ion-icon>
-                </div>
-                <span className="link-text">Settings</span>
+                <Link to="/page4">
+                  <span className="link-text">Team</span>
+                </Link>
               </a>
             </li>
           </ul>
         </nav>
-
-        {/* Main content */}
-        <main id="main" className="flexbox-col">
-          <h2>Lorem ipsum!</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-            corporis, rerum doloremque iste sed voluptates omnis molestias
-            molestiae animi recusandae labore sit amet delectus ad
-            necessitatibus laudantium qui! Magni quisquam illum quaerat
-            necessitatibus sint quibusdam perferendis! Aut ipsam cumque deleniti
-            error perspiciatis iusto accusamus consequuntur assumenda. Obcaecati
-            minima sed natus?
-          </p>
-        </main>
       </div>
     );
   }
