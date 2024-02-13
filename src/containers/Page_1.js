fe27949945035adb3a8be84639f102605aefd15e
Stamp-Xzer <147483678+Stamp-Xzer@users.noreply.github.com>
2024-02-13 22:58:11 +0700

    .

--- src/containers/Page_1.js
+++ src/containers/Page_1.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import withAuth from "../containers/error/auth.js"; // Adjusted import path for withAuth
 import { Link } from "react-router-dom/cjs/react-router-dom.min.js";
+import Headers from "../components/Header.js"; // Adjusted import path for Headers
+import Footer from "../components/Footer.js"; // Adjusted import path for Headers
 
 class Page_1 extends Component {
   render() {
@@ -31,7 +33,7 @@ class Page_1 extends Component {
             <li className="navbar-item flexbox-left">
               <a className="navbar-item-inner flexbox-left">
                 <div className="navbar-item-inner-icon-wrapper flexbox">
-                  <ion-icon name="folder-open-outline"></ion-icon>
+                  <ion-icon name="person-outline"></ion-icon>
                 </div>
                 <Link to="/page2">
                   <span className="link-text">Profile</span>
@@ -41,25 +43,26 @@ class Page_1 extends Component {
             <li className="navbar-item flexbox-left">
               <a className="navbar-item-inner flexbox-left">
                 <div className="navbar-item-inner-icon-wrapper flexbox">
-                  <ion-icon name="pie-chart-outline"></ion-icon>
+                  <ion-icon name="document-text-outline"></ion-icon>
                 </div>
                 <Link to="/page3">
                   <span className="link-text">Subject</span>
                 </Link>
               </a>
             </li>
-            <li className="navbar-item flexbox-left">
-              <a className="navbar-item-inner flexbox-left">
-                <div className="navbar-item-inner-icon-wrapper flexbox">
-                  <ion-icon name="people-outline"></ion-icon>
-                </div>
-                <Link to="/page4">
-                  <span className="link-text">Team</span>
-                </Link>
-              </a>
-            </li>
           </ul>
         </nav>
+
+        {/* Main content */}
+        {/* <div className="container-md">
+          <h1 className="text-center">Welcome to the Home Page</h1>
+          <p className="text-center">
+            This is the home page. You can navigate to other pages using the
+            navigation bar on the left.
+          </p>
+        </div> */}
+        <Headers />
+        <Footer company="Kasetsart University" email="Mathit.j@ku.th" />
       </div>
     );
   }
